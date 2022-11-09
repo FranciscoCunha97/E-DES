8d6e773b988595b3d433fec075a1ad95c1993255
FranciscoCunha97 <francisco.97@live.com.pt>
2022-11-09 23:49:15 +0000

    generator

--- E-DES.py
+++ E-DES.py
@@ -1,5 +1,5 @@
 import hashlib
-import random
+from random import *
 import string
 
 '''
@@ -10,10 +10,30 @@ def generate_key():
 print("key" + generate_key())
 '''
 
-key = "I don't show ID at clubs, 'cause they know that I am 21 - Savage"
-
 def hash_sha256():
     hashed_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
     return hashed_key
 
-print(hash_sha256())
+key = "I don't show ID at clubs, 'cause they know that I am 21 - Savage"
+
+def rc4():
+    hashed_key = hash_sha256()
+    S = [x for x in range(256)]
+    shuffle(S)
+    j=0
+    hashed_key=[ord(x) for x in hashed_key]
+    for i in range(256):
+        j = (j + S[i] + int(hashed_key[i % len(hashed_key)])) % 256
+    
+    """"
+    for S_hex in
+        h = []
+        S_hex = hex(S_hex)
+        h.append(S_hex)
+    #print(len(S))
+    """
+    
+    return S_hex
+
+print(rc4())
+
